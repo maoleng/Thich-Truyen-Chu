4241aede5c76147d1a9d3a03ff1703d718c1e64b
Bùi Hữu Lộc <feature451@gmail.com>
2022-10-22 14:12:15 +0700

    wait the server finish requestting

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -19,7 +19,7 @@ class Kernel extends ConsoleKernel
 
     protected function schedule(Schedule $schedule)
     {
-         $schedule->command('comic:clone')->cron('48 13 * * *');
+//         $schedule->command('comic:clone')->cron('48 13 * * *');
     }
 
     /**
