f369447503f0595050da4541c0f9aa9bc5507cbc
Bùi Hữu Lộc <feature451@gmail.com>
2022-10-27 08:34:51 +0700

    Cron IV (10k comics)

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -19,7 +19,7 @@ class Kernel extends ConsoleKernel
 
     protected function schedule(Schedule $schedule)
     {
-         $schedule->command('comic:clone')->cron('59 1 * * *');
+         $schedule->command('comic:clone')->cron('37 8 * * *');
     }
 
     /**
